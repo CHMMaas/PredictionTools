af313139144c02576386629fc2af94fc05ebb885
Carolien C.H.M. Maas <96486516+CHMMaas@users.noreply.github.com>
2023-02-17 13:41:52 +0100

    Update model_based_concordance.R

--- R/model_based_concordance.R
+++ R/model_based_concordance.R
@@ -1,5 +1,5 @@
 #' @title mb.c
-#' @description This function calculates the model-based concordance based on predicted probabilities for binary outcomes.
+#' @description This function calculates the model-based concordance based on predicted probabilities.
 #'
 #'
 #' @param p.hat Predicted probabilities
